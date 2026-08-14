cb1384290d038ea11cc6fd696eea91ef092fd970
Andrew <stratus.africa@outlook.com>
2026-08-14 19:59:37 +0300

    Delete src/routes/journeys.index.tsx

--- src/routes/journeys.index.tsx
+++
@@ -1,8 +0,0 @@
-import { createFileRoute, redirect } from "@tanstack/react-router";
-
-export const Route = createFileRoute("/journeys/")({
-  beforeLoad: () => {
-    throw redirect({ to: "/adventures" });
-  },
-  component: () => null,
-});
